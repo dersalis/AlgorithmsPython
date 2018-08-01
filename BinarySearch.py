379ab23fc1b63ec21ac877716b6bd430ff62943d
Damian.ek <dersalis@outlook.com>
2018-08-01 22:55:10 +0200

    Binary search

---
+++ BinarySearch.py
@@ -0,0 +1,10 @@
+# Podaj liczbę początkową zakresu
+fNumber = input("Podaj pierwszą liczbę zakresu: ")
+# Podaj liczbę końcową zakresu
+lNumber = input("Podaj ostatnią liczbę zakresu: ")
+# Liczba końcowa musi być większa od początkowej
+if lNuber <= fNumber:
+    print("Ostatnia liczba zakresu musi być większa od pierwszej!")
+else:
+    # Podaj szukaną liczbę 
+    sNumber = input("Podaj szukaną liczbę: ")
